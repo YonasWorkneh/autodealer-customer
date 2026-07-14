b757299d4a5aa94f8ac7021c0a2b71a7285d73c6
YonasWorkneh <yonasdegefu158@gmail.com>
2026-07-14 09:07:34 +0300

    fix: remove redundant favicon links from layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -146,8 +146,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
